952e92ea75c8bea97f1547cdb352a0942e0a451a
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-08 00:33:04 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,8 +23,10 @@ def third_challenge
       :name => "Lady Capulet"
       :age => "51"
         },
-      :heroine => {
-        #your key/value pairs here
+      :heroine => {{:name => "Juliet",
+      :age => "15",
+      :status => "alive"
+        },
         },
       :heroine_friends => []
    }
