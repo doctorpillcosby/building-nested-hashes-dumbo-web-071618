af19f915b13a4cd8fb6f87200c266cd96d1f5d26
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-08 00:20:53 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,12 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {:name => "Lord Montague",
+      :age => "53"
         #your key/value pairs here
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name => "Lady Montague",
+      :age => "54"
         },
       :hero => {
         #your key/value pairs here
